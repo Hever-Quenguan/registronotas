d2b821c22a2e20472a3715f98b789e1dcb8da790
Juan felipe viteri obando <juanfelipeviterimax@gmail.com>
2025-03-31 16:18:44 -0500

    configuracion a Postgres

--- aplicacion/aplicacion/settings.py
+++ aplicacion/aplicacion/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Django REST Framework
+    'rest_framework',
+
+    # Tu app
+    'api',
+    'corsheaders',  # Habilitar CORS
 ]
 
 MIDDLEWARE = [
@@ -75,10 +82,15 @@ WSGI_APPLICATION = 'aplicacion.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'notas',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
