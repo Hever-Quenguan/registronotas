1acec20e2bad42f3acbe70c50be44e86c313fc23
Juan felipe viteri obando <juanfelipeviterimax@gmail.com>
2025-06-02 18:02:04 -0500

    se altero el dashboard: clases en su componente

--- Angular/src/app/pages/dashboards/clases/clases.component.ts
+++ Angular/src/app/pages/dashboards/clases/clases.component.ts
@@ -14,76 +14,76 @@ import { Profesor } from '../../../model/profesores-model';
   imports: [CommonModule, FormsModule, HttpClientModule],
   templateUrl: './clases.component.html',
 })
-export class ClasesComponent implements OnInit {
-  clases: Clase[] = [];
-  clase: Clase = this.resetClase();
-  cursos: Curso[] = [];
-  profesores: Profesor[] = [];
-  editando: boolean = false;
-  claseEditandoId: number | null = null;
+export class ClasesComponent implements OnInit { 
+  clases: Clase[] = []; 
+  clase: Clase = this.resetClase(); 
+  cursos: Curso[] = []; 
+  profesores: Profesor[] = []; 
+  editando: boolean = false; 
+  claseEditandoId: number | null = null; 
 
   constructor(private claseService: ClasesService) {}
 
-  ngOnInit(): void {
-    this.cargarClases();
-    this.cargarCursos();
-    this.cargarProfesores();
+  ngOnInit(): void { 
+    this.cargarClases(); 
+    this.cargarCursos(); 
+    this.cargarProfesores(); 
   }
 
-  cargarClases() {
-    this.claseService.getClases().subscribe(data => this.clases = data);
+  cargarClases() { 
+    this.claseService.getClases().subscribe(data => this.clases = data); 
   }
 
   cargarCursos() {
-    this.claseService.getCursos().subscribe(data => this.cursos = data);
+    this.claseService.getCursos().subscribe(data => this.cursos = data); 
   }
 
   cargarProfesores() {
-    this.claseService.getProfesores().subscribe(data => this.profesores = data);
+    this.claseService.getProfesores().subscribe(data => this.profesores = data); 
   }
 
 guardarClase() {
-  this.clase.curso = Number(this.clase.curso);
-  this.clase.profesor = Number(this.clase.profesor);
+  this.clase.curso = Number(this.clase.curso); 
+  this.clase.profesor = Number(this.clase.profesor); 
 
-  if (this.editando && this.claseEditandoId !== null) {
-    this.claseService.updateClase(this.claseEditandoId, this.clase).subscribe(() => {
-      this.resetFormulario();
-      this.cargarClases();
+  if (this.editando && this.claseEditandoId !== null) { 
+    this.claseService.updateClase(this.claseEditandoId, this.clase).subscribe(() => { 
+      this.resetFormulario(); 
+      this.cargarClases(); 
     });
   } else {
-    this.claseService.addClase(this.clase).subscribe(() => {
-      this.resetFormulario();
-      this.cargarClases();
+    this.claseService.addClase(this.clase).subscribe(() => { 
+      this.resetFormulario(); 
+      this.cargarClases(); 
     });
   }
 }
 
 
-  editar(clase: Clase) {
-    this.clase = { ...clase };
-    this.editando = true;
-    this.claseEditandoId = clase.id ?? null;
+  editar(clase: Clase) { 
+    this.clase = { ...clase }; 
+    this.editando = true; 
+    this.claseEditandoId = clase.id ?? null; 
   }
 
-  eliminar(clase: Clase) {
-    if (confirm('¿Eliminar esta clase?')) {
-      this.claseService.deleteClase(clase.id!).subscribe(() => this.cargarClases());
+  eliminar(clase: Clase) { 
+    if (confirm('¿Eliminar esta clase?')) { 
+      this.claseService.deleteClase(clase.id!).subscribe(() => this.cargarClases()); 
     }
   }
 
   resetFormulario() {
-    this.clase = this.resetClase();
-    this.editando = false;
-    this.claseEditandoId = null;
+    this.clase = this.resetClase(); 
+    this.editando = false; 
+    this.claseEditandoId = null; 
   }
 
-  resetClase(): Clase {
+  resetClase(): Clase { 
     return {
-      curso: 0,
-      profesor: 0,
-      horario: '',
-      semestre: ''
+      curso: 0, 
+      profesor: 0, 
+      horario: '', 
+      semestre: '' 
     };
   }
 }
