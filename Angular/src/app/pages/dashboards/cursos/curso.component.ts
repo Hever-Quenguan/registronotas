61c57202d573b60033ff4edb0bab0830beb2e379
Juan felipe viteri obando <juanfelipeviterimax@gmail.com>
2025-06-02 18:00:12 -0500

    se arreglo un bug en el componente del dashboard: cursos

--- Angular/src/app/pages/dashboards/cursos/curso.component.ts
+++ Angular/src/app/pages/dashboards/cursos/curso.component.ts
@@ -5,64 +5,64 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
-@Component({
-  selector: 'app-cursos',
-  standalone: true,
-  imports: [CommonModule, FormsModule, HttpClientModule],
-  templateUrl: './curso.component.html',
-})
-export class CursosComponent implements OnInit {
-  cursos: Curso[] = [];
-  curso: Curso = this.resetCurso();
-  editando: boolean = false;
-  cursoEditandoId: number | null = null;
+@Component({ 
+  selector: 'app-cursos',  
+  standalone: true, 
+  imports: [CommonModule, FormsModule, HttpClientModule], 
+  templateUrl: './curso.component.html', 
+}) 
+export class CursosComponent implements OnInit { 
+  cursos: Curso[] = []; 
+  curso: Curso = this.resetCurso(); 
+  editando: boolean = false; 
+  cursoEditandoId: number | null = null; 
 
-  constructor(private cursoService: CursosService) {}
+  constructor(private cursoService: CursosService) {} 
 
-  ngOnInit(): void {
-    this.cargarCursos();
-  }
+  ngOnInit(): void { 
+    this.cargarCursos(); 
+  } 
 
-  cargarCursos() {
-    this.cursoService.getCursos().subscribe(data => this.cursos = data);
-  }
+  cargarCursos() { 
+    this.cursoService.getCursos().subscribe(data => this.cursos = data); 
+  } 
 
-  guardarCurso() {
-    if (this.editando && this.cursoEditandoId !== null) {
-      this.cursoService.updateCurso(this.cursoEditandoId, this.curso).subscribe(() => {
-        this.resetFormulario();
-        this.cargarCursos();
+  guardarCurso() { 
+    if (this.editando && this.cursoEditandoId !== null) { 
+      this.cursoService.updateCurso(this.cursoEditandoId, this.curso).subscribe(() => { 
+        this.resetFormulario(); 
+        this.cargarCursos(); 
       });
     } else {
-      this.cursoService.addCurso(this.curso).subscribe(() => {
-        this.resetFormulario();
-        this.cargarCursos();
+      this.cursoService.addCurso(this.curso).subscribe(() => { 
+        this.resetFormulario(); 
+        this.cargarCursos(); 
       });
     }
   }
 
-  editar(curso: Curso) {
-    this.curso = { ...curso };
-    this.editando = true;
-    this.cursoEditandoId = curso.id ?? null;
+  editar(curso: Curso) { 
+    this.curso = { ...curso }; 
+    this.editando = true; 
+    this.cursoEditandoId = curso.id ?? null; 
   }
 
-  eliminar(curso: Curso) {
-    if (confirm('¿Eliminar este curso?') && curso.id) {
-      this.cursoService.deleteCurso(curso.id).subscribe(() => this.cargarCursos());
+  eliminar(curso: Curso) { 
+    if (confirm('¿Eliminar este curso?') && curso.id) { 
+      this.cursoService.deleteCurso(curso.id).subscribe(() => this.cargarCursos()); 
     }
   }
 
-  resetFormulario() {
-    this.curso = this.resetCurso();
-    this.editando = false;
-    this.cursoEditandoId = null;
+  resetFormulario() { 
+    this.curso = this.resetCurso(); 
+    this.editando = false; 
+    this.cursoEditandoId = null; 
   }
 
-  resetCurso(): Curso {
-    return {
-      nombre: '',
-      descripcion: ''
+  resetCurso(): Curso { 
+    return { 
+      nombre: '', 
+      descripcion: '' 
     };
   }
 }
