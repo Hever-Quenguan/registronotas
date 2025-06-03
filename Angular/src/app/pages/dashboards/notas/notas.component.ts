25568bd192487dcd30c645151641ab7000f7dd96
Juan felipe viteri obando <juanfelipeviterimax@gmail.com>
2025-06-02 19:09:58 -0500

    se altero el ts de :notas

--- Angular/src/app/pages/dashboards/notas/notas.component.ts
+++ Angular/src/app/pages/dashboards/notas/notas.component.ts
@@ -8,80 +8,80 @@ import { Nota } from 'src/app/model/notas-model';
 import { Estudiante } from 'src/app/core/services/estudiante.service';
 import { Evaluacion } from 'src/app/model/evaluacion-model';
 
-@Component({
-  selector: 'app-notas',
-  standalone: true,
-  imports: [CommonModule, FormsModule, HttpClientModule],
-  templateUrl: './notas.component.html',
-})
-export class NotasComponent implements OnInit {
-  notas: Nota[] = [];
-  nota: Nota = this.resetNota();
-  estudiantes: Estudiante[] = [];
-  evaluaciones: Evaluacion[] = [];
-  editando: boolean = false;
-  notaEditandoId: number | null = null;
+@Component({ 
+  selector: 'app-notas', 
+  standalone: true, 
+  imports: [CommonModule, FormsModule, HttpClientModule], 
+  templateUrl: './notas.component.html', 
+}) 
+export class NotasComponent implements OnInit { 
+  notas: Nota[] = []; 
+  nota: Nota = this.resetNota(); 
+  estudiantes: Estudiante[] = []; 
+  evaluaciones: Evaluacion[] = []; 
+  editando: boolean = false; 
+  notaEditandoId: number | null = null; 
 
-  constructor(private notaService: NotaService) {}
+  constructor(private notaService: NotaService) {} 
 
-  ngOnInit(): void {
-    this.cargarNotas();
-    this.cargarEstudiantes();
-    this.cargarEvaluaciones();
+  ngOnInit(): void { 
+    this.cargarNotas(); 
+    this.cargarEstudiantes(); 
+    this.cargarEvaluaciones(); 
   }
+ 
+  cargarNotas() { 
+    this.notaService.getNotas().subscribe(data => this.notas = data); 
+  } 
 
-  cargarNotas() {
-    this.notaService.getNotas().subscribe(data => this.notas = data);
-  }
+  cargarEstudiantes() { 
+    this.notaService.getEstudiantes().subscribe(data => this.estudiantes = data); 
+  } 
 
-  cargarEstudiantes() {
-    this.notaService.getEstudiantes().subscribe(data => this.estudiantes = data);
-  }
+  cargarEvaluaciones() { 
+    this.notaService.getEvaluaciones().subscribe(data => this.evaluaciones = data); 
+  } 
 
-  cargarEvaluaciones() {
-    this.notaService.getEvaluaciones().subscribe(data => this.evaluaciones = data);
-  }
+  guardarNota() { 
+    this.nota.estudiante = Number(this.nota.estudiante); 
+    this.nota.evaluacion = Number(this.nota.evaluacion); 
 
-  guardarNota() {
-    this.nota.estudiante = Number(this.nota.estudiante);
-    this.nota.evaluacion = Number(this.nota.evaluacion);
-
-    if (this.editando && this.notaEditandoId !== null) {
-      this.notaService.updateNota(this.notaEditandoId, this.nota).subscribe(() => {
-        this.resetFormulario();
-        this.cargarNotas();
-      });
-    } else {
-      this.notaService.addNota(this.nota).subscribe(() => {
-        this.resetFormulario();
-        this.cargarNotas();
-      });
+    if (this.editando && this.notaEditandoId !== null) { 
+      this.notaService.updateNota(this.notaEditandoId, this.nota).subscribe(() => { 
+        this.resetFormulario(); 
+        this.cargarNotas(); 
+      }); 
+    } else {  
+      this.notaService.addNota(this.nota).subscribe(() => { 
+        this.resetFormulario(); 
+        this.cargarNotas(); 
+      }); 
     }
   }
 
-  editar(nota: Nota) {
-    this.nota = { ...nota };
-    this.editando = true;
-    this.notaEditandoId = nota.id ?? null;
+  editar(nota: Nota) { 
+    this.nota = { ...nota }; 
+    this.editando = true; 
+    this.notaEditandoId = nota.id ?? null; 
+  } 
+
+  eliminar(nota: Nota) { 
+    if (confirm('¿Eliminar esta nota?')) { 
+      this.notaService.deleteNota(nota.id!).subscribe(() => this.cargarNotas()); 
+    } 
+  } 
+
+  resetFormulario() { 
+    this.nota = this.resetNota(); 
+    this.editando = false; 
+    this.notaEditandoId = null; 
   }
 
-  eliminar(nota: Nota) {
-    if (confirm('¿Eliminar esta nota?')) {
-      this.notaService.deleteNota(nota.id!).subscribe(() => this.cargarNotas());
-    }
-  }
-
-  resetFormulario() {
-    this.nota = this.resetNota();
-    this.editando = false;
-    this.notaEditandoId = null;
-  }
-
-  resetNota(): Nota {
-    return {
-      estudiante: 0,
-      evaluacion: 0,
-      calificacion: 0
-    };
-  }
+  resetNota(): Nota { 
+    return { 
+      estudiante: 0, 
+      evaluacion: 0, 
+      calificacion: 0 
+    }; 
+  } 
 }
